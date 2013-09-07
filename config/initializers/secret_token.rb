275758a1b5ef4e275cc35d7a6446a0584075d42a
Rohit Kabadi <rkabadi06@gmail.com>
2013-09-07 14:30:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StrokeFinder::Application.config.secret_key_base = 'ac842364ad90da6b2347f3c289d677629fd4211a218904044828e422f2efaef708f446ab4db117122a4fcd5155f0947eb7d4beef622f19a58e0eb082c7efe42d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StrokeFinder::Application.config.session_store :cookie_store, key: '_stroke_finder_session'
